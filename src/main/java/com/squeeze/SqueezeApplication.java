a63831cdecbc079c2a23e25ec24e4ae02d52266d
tiagomartinho <tiago.martinho@xpeppers.com>
2016-02-28 11:17:19 +0100

    initial commit

---
+++ src/main/java/com/squeeze/SqueezeApplication.java
@@ -0,0 +1,12 @@
+package com.squeeze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqueezeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqueezeApplication.class, args);
+	}
+}
